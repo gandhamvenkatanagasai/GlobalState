3a43c6b32508991c721cb381b5265b64989176ff
Sai <venkata-naga-sai.gandham@capgemini.com>
2019-02-14 10:55:07 +0530

    Added Redis Extension Files

--- GlobalState.API/Repository/UserOperationRepo.cs
+++ GlobalState.API/Repository/UserOperationRepo.cs
@@ -14,7 +14,11 @@ namespace GlobalState.API.Repository
 {
     public class UserOperationRepo : IUserOperationRepo
     {
-        IDatabase cache = RedisConnectorHelper.Connection.GetDatabase();
+        //Redis Helper Classes
+        private static readonly RedisHelper RedisConnection = new RedisHelper();
+        //Use in each method when doing the Redis Operation
+        private static readonly IDatabase _redisExtension = RedisConnection.GetConnection();
+
         static IDbConnection db = new SqlConnection(Environment.GetEnvironmentVariable("SqlConnectionString"));
 
         public string GetVersion()
@@ -35,8 +39,10 @@ namespace GlobalState.API.Repository
           
                 try
                 {
+                
 
-                    db.Open();                
+
+                db.Open();                
                     var parameter = new DynamicParameters();
                     parameter.Add("@Id", userAccountDetail.LId);
                     parameter.Add("@action", userAccountDetail.action);
@@ -44,23 +50,23 @@ namespace GlobalState.API.Repository
                     parameter.Add("@EmailId", userAccountDetail.EmailId);
                     parameter.Add("@Password", userAccountDetail.Password);
                     return (await db.QueryAsync<UserAccountDetail>("USP_UserAccount", parameter, commandType: CommandType.StoredProcedure)).AsList();
-                    //con.Open();
-                    //SqlCommand command = new SqlCommand("USP_UserAccount", con);
-                    //command.CommandType = System.Data.CommandType.StoredProcedure;
-                    //command.Parameters.AddWithValue("@action", userAccountDetail.action);
-                    //command.Parameters.AddWithValue("@UserName", userAccountDetail.UserName);
-                    //command.Parameters.AddWithValue("@Password", userAccountDetail.Password);
-                    //command.Parameters.AddWithValue("@EmailId", userAccountDetail.EmailId);
-                    //command.ExecuteScalar();
-                    //return new UserAccountDetail();
+                //con.Open();
+                //SqlCommand command = new SqlCommand("USP_UserAccount", con);
+                //command.CommandType = System.Data.CommandType.StoredProcedure;
+                //command.Parameters.AddWithValue("@action", userAccountDetail.action);
+                //command.Parameters.AddWithValue("@UserName", userAccountDetail.UserName);
+                //command.Parameters.AddWithValue("@Password", userAccountDetail.Password);
+                //command.Parameters.AddWithValue("@EmailId", userAccountDetail.EmailId);
+                //command.ExecuteScalar();
+                //return new UserAccountDetail();
 
-                    //DynamicParameters parameters = new DynamicParameters();
-                    //parameters.Add("@UserName", userAccountDetail.UserName);
-                    //parameters.Add("@Password", userAccountDetail.Password);
-                    //parameters.Add("@EmailId", userAccountDetail.EmailId);
-                    //var result = await SqlMapper.QueryAsync<UserAccountDetail>((SqlConnection)con,
-                    //  "uspUserRegistration", parameters, commandType: StoredProcedure);
-                    //return result.FirstOrDefault();
+                //DynamicParameters parameters = new DynamicParameters();
+                //parameters.Add("@UserName", userAccountDetail.UserName);
+                //parameters.Add("@Password", userAccountDetail.Password);
+                //parameters.Add("@EmailId", userAccountDetail.EmailId);
+                //var result = await SqlMapper.QueryAsync<UserAccountDetail>((SqlConnection)con,
+                //  "uspUserRegistration", parameters, commandType: StoredProcedure);
+                //return result.FirstOrDefault();
 
                 }
                 catch (Exception ex)
---
+++ GlobalState.API/Utilities/RedisHelper.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace GlobalState.API.Utilities
+{
+    public class RedisHelper : IRedisHelper
+    {
+        private static readonly Lazy<ConnectionMultiplexer> LazyConnection = new Lazy<ConnectionMultiplexer>(()
+            => ConnectionMultiplexer.Connect(Environment.GetEnvironmentVariable("RedisConnectionString")));
+
+        private static ConnectionMultiplexer Connection => LazyConnection.Value;
+
+        public IDatabase GetConnection()
+        {
+            return Connection.GetDatabase();
+        }
+
+    }
+
+    public interface IRedisHelper
+    {
+        IDatabase GetConnection();
+    }
+}
